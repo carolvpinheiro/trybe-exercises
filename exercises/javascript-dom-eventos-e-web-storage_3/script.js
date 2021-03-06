c98299b7a415471d942cb94563e3dda5b2d5eb6f
Carol <carolinavpinheiro@gmail.com>
2021-03-06 14:55:37 -0300

    Adicionando correções ao exercício 1

--- exercises/javascript-dom-eventos-e-web-storage_3/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_3/script.js
@@ -1,4 +1,4 @@
-function createDaysOfTheWeek() {
+  function createDaysOfTheWeek() {
     const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
     const weekDaysList = document.querySelector('.week-days');
   
@@ -10,19 +10,71 @@ function createDaysOfTheWeek() {
       weekDaysList.appendChild(dayListItem);
     };
   };
-  
   createDaysOfTheWeek();
   
   function createDaysOfTheMonth() {
     const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
     
     for (let index = 0; index < dezDaysList.length; index += 1) {
-      const numberDays = dezDaysList[index];
+      const numbersDays = dezDaysList[index];
       const daysListNumber = document.createElement('li');
-      daysListNumber.className = 'day';
-      daysListNumber.innerHTML = numberDays;
-
+      daysListNumber.innerHTML = numbersDays;
       document.querySelector('#days').appendChild(daysListNumber);
+
+      daysListNumber.className = 'day';
+      
+      const holidayDays = [24, 25, 31];
+      for (let indexHoliday = 0; indexHoliday < holidayDays.length; indexHoliday += 1) {
+        const numbersHoliday = holidayDays[indexHoliday];
+        if (numbersDays === numbersHoliday) {
+          daysListNumber.className = 'holiday';
+        }
+      }
+
+      const fridayDays = [4, 11, 18, 25];
+      for (let indexFriday = 0; indexFriday < fridayDays.length; indexFriday += 1) {
+        const numbersFridayDays = fridayDays[indexFriday];
+        if (numbersDays === numbersFridayDays) {
+          daysListNumber.className = 'day-friday';
+        }
+      }
     }
   }
   createDaysOfTheMonth();
+  console.log(document.querySelectorAll('.holiday'));
+  console.log(document.querySelectorAll('.day-friday'));
+  console.log(document.querySelectorAll('.day'));
+
+  /*
+ 
+  function createHolidayDays() {
+    const holidayDays = [24, 25, 31];
+    let daysMonth = document.querySelectorAll('.day');
+
+    for (let index = 0; index < daysMonth.length; index += 1) {
+      for (let indexHoliday = 0; indexHoliday < holidayDays.length; indexHoliday += 1) {
+        if (daysMonth[index] === holidayDays[indexHoliday]) {
+          daysMonth.className = 'day-holiday'
+        }
+      } 
+    }
+  }
+  console.log(createHolidayDays());
+  console.log(document.querySelectorAll('.day'));
+  
+  function createFridayDays() {
+    const fridayDays = [4, 11, 18, 25];
+    let daysMonth = document.querySelectorAll('.day');
+
+    for (let key in daysMonth) {
+      let eachDayMonth = daysMonth[key];
+      for(let index = 0; index < fridayDays.length; index += 1) {
+        if (eachDayMonth === fridayDays[index]) {
+          eachDayMonth.className = 'day-friday';
+        }
+      }
+    }
+  }
+  console.log(createFridayDays());
+  console.log(document.querySelectorAll('.day'));
+*/  
