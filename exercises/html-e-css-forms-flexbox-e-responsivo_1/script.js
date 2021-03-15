0a073a4be4b2079cf705afc3c0c032bf3499fb94
Carol <carolinavpinheiro@gmail.com>
2021-03-15 20:22:06 -0300

    Adicionando exercício 1 e o 2 parcialmente

---
+++ exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -0,0 +1,39 @@
+const statesOfBrazil = {
+  'AC': 'Acre',
+  'AL': 'Alagoas',
+  'AP': 'Amapá',
+  'AM': 'Amazonas',
+  'BA': 'Bahia',
+  'CE': 'Ceará',
+  'DF': 'Distrito Federal',
+  'ES': 'Espírito Santo',
+  'GO': 'Goías',
+  'MA': 'Maranhão',
+  'MT': 'Mato Grosso',
+  'MS': 'Mato Grosso do Sul',
+  'MG': 'Minas Gerais',
+  'PA': 'Pará',
+  'PB': 'Paraíba',
+  'PR': 'Paraná',
+  'PE': 'Pernambuco',
+  'PI': 'Piauí',
+  'RJ': 'Rio de Janeiro',
+  'RN': 'Rio Grande do Norte',
+  'RS': 'Rio Grande do Sul',
+  'RO': 'Rondônia',
+  'RR': 'Roraíma',
+  'SC': 'Santa Catarina',
+  'SP': 'São Paulo',
+  'SE': 'Sergipe',
+  'TO': 'Tocantins'
+};
+
+function createStateList(states) {
+  for (let key in states) {
+    let option = document.createElement('option');
+    document.querySelector('#state').appendChild(option);
+    option.value = key;
+    option.innerHTML = states[key];
+  }
+}
+createStateList(statesOfBrazil);
