a60b10a4f3a106dde1dfa3e7ee46dab59eb3e254
Carol <carolinavpinheiro@gmail.com>
2021-03-16 18:27:51 -0300

    Adicionando tentativa de validação de data

--- exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
+++ exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -1,3 +1,4 @@
+/*
 const statesOfBrazil = {
   'AC': 'Acre',
   'AL': 'Alagoas',
@@ -27,6 +28,7 @@ const statesOfBrazil = {
   'SE': 'Sergipe',
   'TO': 'Tocantins'
 };
+//Referência: https://gist.github.com/kavalcante/16106796c430c1711a71c5ba1213cee5#gistcomment-3432282
 
 function createStateList(states) {
   for (let key in states) {
@@ -37,3 +39,53 @@ function createStateList(states) {
   }
 }
 createStateList(statesOfBrazil);
+*/
+let startDate = document.createElement('input');
+document.querySelector('#start-date').appendChild(startDate);
+startDate.type = "text";
+startDate.name = "start-date";
+startDate.id = "start-date";
+startDate.maxLength = "10";
+startDate.placeholder = "Digite a data no formato dd/mm/aaaa";
+startDate.required = true;
+
+let button = document.querySelector('#submit');
+
+button.addEventListener('click', () => {
+  console.log(startDate.innerText);
+});
+
+function dateVerification(date) {
+  let dateArray = date.split("/");
+  console.log(dateArray)
+  let dateDay = dateArray[0];
+  let dateMonth = dateArray[1];
+  let dateYear = dateArray[2];
+
+  if (dateArray === null) {
+    return false;
+  }
+
+  if (dateDay > 0 && dateDay <= 31) {
+    return true;
+  } else if (dateMonth > 0 && dateMonth <= 12) {
+    return true;
+  } else if (dateYear > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+dateVerification();
+
+console.log(dateVerification("1203"))
+
+if (dateVerification(data) === true) {
+  //startDate.innerHTML = date;
+  console.log(date);
+} else {
+  //startDate.innerHTML = 'Data inválida';
+  console.log('Data inválida');
+}
+
+//Referência: https://pt.stackoverflow.com/questions/91379/como-validar-data-com-angularjs-ou-jquery
