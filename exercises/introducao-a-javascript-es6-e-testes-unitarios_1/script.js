db916f77c6ad37cf3db5acad2b86987b450f9605
Carol <carolinavpinheiro@gmail.com>
2021-03-23 20:17:18 -0300

    Adicionando modificações

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/script.js
@@ -1,7 +1,6 @@
 // 1 - Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
 // Modifique a estrutura da função para que ela seja uma arrow function .
 // Modifique as concatenações para template literals .
-// Copie o código abaixo.
 
 const testingScope = (escopo) => {
   if (escopo === true) {
@@ -16,3 +15,17 @@ const testingScope = (escopo) => {
 }
 
 testingScope(true);
+
+// 2 - Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+// Utilize template literals para que a chamada console.log(oddsAndEvens); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const ascendingOrder = () => {
+  let newArray = [];
+  for (let index = 0; index < oddsAndEvens.length; index +=1) {
+  }
+}
+
+console.log(oddsAndEvens);
