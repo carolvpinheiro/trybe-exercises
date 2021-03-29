4a1c9f1ff5a4af227013122b0486a7d5c6050b11
Carol <carolinavpinheiro@gmail.com>
2021-03-29 20:39:01 -0300

    Adicionando exercicio 2

---
+++ exercises/higher-order-functions-do-javascript-es8.1/script1.js
@@ -0,0 +1,16 @@
+// Crie uma função de primeira classe que retorna um objeto { nomeCompleto, email } de uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respctivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
+
+const pessoaContratada = (nome) => {
+    let nomeEmail = nome.replace(' ', '_').toLowerCase();
+    return { Nome: nome, Email: `${nomeEmail}@trybe.com` };
+}
+
+const newEmployees = () => {
+  const employees = {
+    id1: pessoaContratada('Pedro Guerra'),
+    id2: pessoaContratada('Luiza Drumond'),
+    id3: pessoaContratada('Carla Paiva'),
+  }
+  return employees;
+};
+console.log(newEmployees());
