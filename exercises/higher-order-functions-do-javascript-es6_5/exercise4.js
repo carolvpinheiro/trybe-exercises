ebf9574ac3e8b5573a96bd60dc7a1081d2124491
Carol <carolinavpinheiro@gmail.com>
2021-04-07 19:38:06 -0300

    Adicionando exercício 5

---
+++ exercises/higher-order-functions-do-javascript-es6_5/exercise4.js
@@ -0,0 +1,39 @@
+const assert = require('assert');
+const { brotliCompress } = require('node:zlib');
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+const filterPeople = (arr) => arr.filter(({bornIn, nationality}) =>
+  bornIn > 1990 && bornIn <= 2000 && nationality === 'Australian'
+);
+
+const filteredPeople = filterPeople(people);
+
+assert.deepStrictEqual(filteredPeople[0], { name: 'Nicole', bornIn: 1992, nationality: 'Australian' });
+assert.deepStrictEqual(filteredPeople[1], { name: 'Toby', bornIn: 1901, nationality: 'Australian' });
